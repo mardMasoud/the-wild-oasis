6cafba648b283026ec2b0003beb95f228b4af8ec
abc <you@example.com>
2024-01-16 19:30:58 +0330

    version_upload

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -1,5 +1,3 @@
-import styled from "styled-components";
-
 import Input from "../../ui/Input";
 import Form from "../../ui/Form";
 import Button from "../../ui/Button";
@@ -10,41 +8,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createCabin } from "../../services/apiCabins";
 import toast from "react-hot-toast";
 import Spinner from "../../ui/Spinner";
-const FormRow = styled.div`
-    display: grid;
-    align-items: center;
-    grid-template-columns: 24rem 1fr 1.2fr;
-    gap: 2.4rem;
-
-    padding: 1.2rem 0;
-
-    &:first-child {
-        padding-top: 0;
-    }
-
-    &:last-child {
-        padding-bottom: 0;
-    }
-
-    &:not(:last-child) {
-        border-bottom: 1px solid var(--color-grey-100);
-    }
-
-    &:has(button) {
-        display: flex;
-        justify-content: flex-end;
-        gap: 1.2rem;
-    }
-`;
-
-const Label = styled.label`
-    font-weight: 500;
-`;
-
-const Error = styled.span`
-    font-size: 1.4rem;
-    color: var(--color-red-700);
-`;
+import FormRow from "../../ui/FormRow";
 
 function CreateCabinForm() {
     const queryClient = useQueryClient();
@@ -64,8 +28,8 @@ function CreateCabinForm() {
     });
 
     function onSubmit(data) {
-        // mutate(data);
-        console.log(data)
+      //console.log(data)
+     mutate({...data,image:data.image[0]})
     }
 
     function onError(err) {
@@ -74,18 +38,15 @@ function CreateCabinForm() {
     if (isCreating) return <Spinner />;
     return (
         <Form onSubmit={handleSubmit(onSubmit, onError)}>
-            <FormRow>
-                <Label htmlFor="name">Cabin name</Label>
+            <FormRow label="Cabin name" error={errors?.name?.message}>
                 <Input
                     type="text"
                     id="name"
                     {...register("name", { required: "This feild is required" })}
                 />
-                {errors?.name?.message && <Error>{errors.name.message}</Error>}
             </FormRow>
 
-            <FormRow>
-                <Label htmlFor="maxCapacity">Maximum capacity</Label>
+            <FormRow error={errors?.maxCapacity?.message} label="Maximum capacity">
                 <Input
                     type="number"
                     id="maxCapacity"
@@ -97,11 +58,9 @@ function CreateCabinForm() {
                         },
                     })}
                 />
-                {errors?.maxCapacity?.message && <Error>{errors.maxCapacity.message}</Error>}
             </FormRow>
 
-            <FormRow>
-                <Label htmlFor="regularPrice">Regular price</Label>
+            <FormRow label="Regular price" error={errors?.regularPrice?.message}>
                 <Input
                     type="number"
                     id="regularPrice"
@@ -113,11 +72,9 @@ function CreateCabinForm() {
                         },
                     })}
                 />
-                {errors?.regularPrice?.message && <Error>{errors.regularPrice.message}</Error>}
             </FormRow>
 
-            <FormRow>
-                <Label htmlFor="discount">Discount</Label>
+            <FormRow label="Discount" error={errors?.discount?.message}>
                 <Input
                     type="number"
                     id="discount"
@@ -128,24 +85,23 @@ function CreateCabinForm() {
                             "Discount should be less than regular price",
                     })}
                 />
-                {errors?.discount?.message && <Error>{errors.discount.message}</Error>}
             </FormRow>
 
-            <FormRow>
-                <Label htmlFor="description">Description for website</Label>
+            <FormRow label="Description for website" error={errors?.description?.message}>
                 <Textarea
                     type="number"
                     id="description"
                     defaultValue=""
                     {...register("description", { required: "This feild is required" })}
                 />
-                {errors?.description?.message && <Error>{errors.description.message}</Error>}
             </FormRow>
 
-            <FormRow>
-                <Label htmlFor="image">Cabin photo</Label>
-                <FileInput id="image" accept="image/*" {...register("image", { required: "This feild is required" })}/>
-                {errors?.image?.message && <Error>{errors.image.message}</Error>}
+            <FormRow label="Cabin photo" error={errors?.image?.message}>
+                <FileInput
+                    id="image"
+                    accept="image/*"
+                    {...register("image", { required: "This feild is required" })}
+                />
             </FormRow>
 
             <FormRow>
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,5 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
+
 export async function getCabins() {
     let { data, error } = await supabase.from("cabins").select("*");
 
@@ -17,10 +18,29 @@ export async function deleteCabin(id) {
     return data;
 }
 export async function createCabin(newCabin) {
-    const { data, error } = await supabase.from("cabins").insert([newCabin]).select();
+   
+    const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll("/", "");
+    // https://rwwrahdstdubttudqayz.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg
+    const imagePath = `${supabaseUrl}//storage/v1/object/public/cabin-images/${imageName}`;
+    // 1.Create cabin
+    const { data, error } = await supabase
+        .from("cabins")
+        .insert([{ ...newCabin, image: imagePath }])
+        .select();
     if (error) {
         console.error(error);
         throw new Error("Cabins could not be created");
+    }
+
+    // 2. Upload image
+    const { error: storageError } = await supabase.storage
+        .from("cabin-images")
+        .upload(imageName, newCabin.image);
+
+    //3. delete
+    if (storageError) {
+        await supabase.from("cabins").delete().eq("id", data.id);
+        throw new Error("Cabins image could not be deleted");
     }
     return data;
 }
